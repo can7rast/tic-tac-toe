6dfb4e6b80e9ee59b2983ac3b9f7d0760d20e6a8
candidas <airat12tlt@mail.ru>
2026-03-05 14:34:14 +0300

    Delete src directory

--- src/internal/domain/gameRepo.go
+++
@@ -1,9 +0,0 @@
-package domain
-
-import "github.com/google/uuid"
-
-type GameRepository interface {
-	Save(game Game) error
-	Get(id uuid.UUID) (Game, error)
-	ShowAllAvailableGames() ([]Game, error)
-}
